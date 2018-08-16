98bbcef9f7770eafb1e618903dd4e6d3fcb2ac26
Patryk Siepka <patrykeo001@gmail.com>
2018-08-16 19:15:48 +0200

    Secound commit

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,4 @@ from flask import render_template
 @app.route('/')
 def index():
     user = {'username':'Patryk'}
-    return render_template('index.html', user=user)
+    return render_template('index.html', user=user, title='HomePage')
