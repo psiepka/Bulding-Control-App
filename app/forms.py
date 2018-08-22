17eea5db07b0eaae719d761dc4aaf01fcf9b41ee
Patryk Siepka <patrykeo001@gmail.com>
2018-08-22 19:44:27 +0200

    Fourth commit i hope i type this good

--- app/forms.py
+++ app/forms.py
@@ -3,23 +3,24 @@ from wtforms import StringField, RadioField, IntegerField, PasswordField, Boolea
 from wtforms.validators import DataRequired, Email
 
 
-class Registration(FlaskForm):
+class RegistrationForm(FlaskForm):
     '''
     This is registraton form where we must type our nickname, name, surname, 
     '''
-    nickname = StringField('Nickname', validators=['DataRequired'])
-    name = StringField('Name', validators=['DataRequired'])
-    surname = StringField('Surname', validators=['DataRequired'])
-    position = RadioField('Position', validators=['DataRequired'], choices=[('value','build'), ('value_2','office')])
-    email = StringField('Email', validators=['DataRequired','Email'])
-    phone = IntegerField('Phone number', validators=['DataRequired'])
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    position = RadioField('Position', validators=[DataRequired()], choices=[('value','build'), ('value_2','office')])
+    email = StringField('Email', validators=[DataRequired(),'Email'])
+    phone = IntegerField('Phone number', validators=[DataRequired()])
+    submit = SubmitField('Register in')
 
 
-class Login(FlaskForm):
+class LoginForm(FlaskForm):
     """
     This is login form where we must type our nickname
     """
-    nickname = StringField('Nickname', validators=['DataRequired'])
-    nickname = PasswordField('Password', validators=['DataRequired'])
+    nickname = StringField('Nickname', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remeber_me = BooleanField('Remeber me')
     submit = SubmitField('Log in')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm, RegistrationForm
 
 @app.route('/')
 def index():
     user = {'username':'Patryk'}
     return render_template('index.html', user=user, title='HomePage')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Congrats now you are logged as {}, remeber me={}'.format(form.nickname.data, form.remeber_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form, title='Log in' )
+
+
+@app.route('/registration')
+def registration():
+    form = RegistrationForm()
+    return render_template('registration.html', form=form, title='Register in ')
+
 
